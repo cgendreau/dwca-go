7b36b8afac207a85b65afac110691295088eb30c
Christian Gendreau <cgendreau@gbif.org>
2018-08-21 22:44:56 -0400

    added new Open on *DwcFile

--- pkg/dwc/dwca.go
+++ pkg/dwc/dwca.go
@@ -1,45 +1,90 @@
 package dwc
 
 import (
+	"encoding/csv"
 	"encoding/xml"
 	"errors"
 	"os"
 	"path/filepath"
+	"strconv"
+	"unicode/utf8"
 )
 
 const (
-	metaXmlFileName = "meta.xml"
+	MetaXMLFileName = "meta.xml"
 )
 
 type Field struct {
-	Index string `xml:"index,attr"`
+	Index byte   `xml:"index,attr"`
 	Term  string `xml:"term,attr"`
 }
 
+// DwcFile represents the definition of a single file within an archive
 type DwcFile struct {
-	Encoding           string `xml:"encoding,attr"`
-	FieldsTerminatedBy string `xml:"fieldsTerminatedBy,attr"`
-	LinesTerminatedBy  string `xml:"linesTerminatedBy,attr"`
-	FieldsEnclosedBy   string `xml:"fieldsEnclosedBy,attr"`
-	IgnoreHeaderLines  string `xml:"ignoreHeaderLines,attr"`
-	RowType            string `xml:"rowType,attr"`
+	Encoding           string   `xml:"encoding,attr"`
+	FieldsTerminatedBy string   `xml:"fieldsTerminatedBy,attr"`
+	LinesTerminatedBy  string   `xml:"linesTerminatedBy,attr"`
+	FieldsEnclosedBy   string   `xml:"fieldsEnclosedBy,attr"`
+	IgnoreHeaderLines  string   `xml:"ignoreHeaderLines,attr"`
+	RowType            string   `xml:"rowType,attr"`
+	Files              []string `xml:"files>location"`
+	Fields             []Field  `xml:"field"`
+	FieldsMap          map[string]byte
+	path               string
+	file               *os.File
 }
 
+//DwcArchive represents the metadata of a DarwinCore Archive (how the data is structured inside the archive)
 type DwcArchive struct {
 	Core struct {
 		DwcFile
-		Id     Field   `xml:"id"`
-		Fields []Field `xml:"field"`
+		ID Field `xml:"id"`
 	} `xml:"core"`
 	Extension []struct {
 		DwcFile
-		CoreId Field   `xml:"coreid"`
-		Fields []Field `xml:"field"`
+		CoreID Field `xml:"coreid"`
 	} `xml:"extension"`
 	Metadata string `xml:"metadata,attr"`
+	path     string
 }
 
-func Open(name string) (*DwcArchive, error) {
+func (file *DwcArchive) postDecode(rootPath string) error {
+	file.path = rootPath
+
+	if err := file.Core.postDecode(rootPath); err != nil {
+		return err
+	}
+
+	if file.Extension != nil {
+		for _, ext := range file.Extension {
+			if err := ext.postDecode(rootPath); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (dwcFile *DwcFile) postDecode(rootPath string) error {
+	if dwcFile.Fields == nil {
+		return nil
+	}
+	dwcFile.FieldsMap = make(map[string]byte)
+	for _, f := range dwcFile.Fields {
+		dwcFile.FieldsMap[f.Term] = f.Index
+	}
+
+	if len(dwcFile.Files) > 1 {
+		return errors.New("dwc package only supports 1 location per file")
+	}
+
+	dwcFile.path = filepath.Join(rootPath, dwcFile.Files[0])
+
+	return nil
+}
+
+// NewDwcArchive opens a folder, reads the meta xml file and returns an initialized *DwcArchive
+func NewDwcArchive(name string) (*DwcArchive, error) {
 	fi, err := os.Stat(name)
 	if err != nil {
 		return nil, err
@@ -49,16 +94,57 @@ func Open(name string) (*DwcArchive, error) {
 		return nil, errors.New("This function only accepts folder for now")
 	}
 
-	file, err := os.Open(filepath.Join(name, metaXmlFileName))
+	//open meta.xml file to read the description of the archive
+	file, err := os.Open(filepath.Join(name, MetaXMLFileName))
+
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var metaxml DwcArchive
-	err2 := xml.NewDecoder(file).Decode(&metaxml)
-	if err2 != nil {
+	dwca := &DwcArchive{}
+	if err := xml.NewDecoder(file).Decode(dwca); err != nil {
 		return nil, err
 	}
-	return &metaxml, nil
+
+	if err := dwca.postDecode(name); err != nil {
+		return nil, err
+	}
+
+	return dwca, nil
+}
+
+// IndexOf returns the index of term within the archive
+func (dwcFile DwcFile) IndexOf(term string) byte {
+	return dwcFile.FieldsMap[term]
+}
+
+//Open returns a new csv *Reader for the current DwcFile
+func (dwcFile *DwcFile) Open() (*csv.Reader, error) {
+
+	file, err := os.Open(dwcFile.path)
+	if err != nil {
+		return nil, err
+	}
+	// keep the pointer, we will close it later
+	dwcFile.file = file
+
+	csvReader := csv.NewReader(file)
+
+	//Handle the field delimiter
+	newstr, err := strconv.Unquote(`"` + dwcFile.FieldsTerminatedBy + `"`)
+	fieldDelimiter, _ := utf8.DecodeRuneInString(newstr)
+	csvReader.Comma = fieldDelimiter
+
+	csvReader.LazyQuotes = true
+	csvReader.FieldsPerRecord = -1
+
+	return csvReader, nil
+}
+
+//Close closes the underlying resource
+func (dwcFile *DwcFile) Close() {
+	if dwcFile.file != nil {
+		dwcFile.file.Close()
+	}
 }
--- pkg/dwc/dwca_test.go
+++ pkg/dwc/dwca_test.go
@@ -1,16 +1,65 @@
 package dwc
 
 import (
+	"io"
+	"log"
 	"testing"
 )
 
-func TestOpen(t *testing.T) {
-	dwca, err := Open("testdata")
+func TestNewDwcArchive(t *testing.T) {
+	dwca, err := NewDwcArchive("testdata")
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if dwca.Core.RowType != "http://rs.tdwg.org/dwc/terms/Occurrence" {
 		t.Fatal("RowType Occurrence Expected")
+	}
+
+	if dwca.Core.Files[0] != "occurrence.txt" {
+		t.Fatal("File ocurrence.txt expected, got " + dwca.Core.Files[0])
+	}
+
+	if len(dwca.Extension) != 2 {
+		t.Fatal("Expected 2 extensions")
+	}
+
+	if dwca.Extension[0].Files[0] != "identification.txt" {
+		t.Fatal("First extension file identification.txt expected, got " + dwca.Extension[0].Files[0])
+	}
+
+	if dwca.Core.IndexOf("http://rs.tdwg.org/dwc/terms/datasetID") != 6 {
+		t.Fatal("datasetID Index should be 6 ")
+	}
+}
+
+func TestDwcFileReader(t *testing.T) {
+	testFolder := "testdata"
+
+	dwca, err := NewDwcArchive(testFolder)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	csvReader, err := dwca.Core.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dwca.Core.Close()
+
+	numberOfLine := 0
+	for {
+		_, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		numberOfLine++
+	}
+
+	if numberOfLine != 21 {
+		t.Fatal("File should should have 21 lines")
 	}
 }
